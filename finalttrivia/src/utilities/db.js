fb7850adeeaaaa9b308adce1a6d04eaecd6b6685
Mansib Rahman <60615362+mvnsib@users.noreply.github.com>
2022-01-05 23:40:49 +0000

    Update db.js

--- finalttrivia/src/utilities/db.js
+++ finalttrivia/src/utilities/db.js
@@ -1,1 +1,8 @@
 import {db} from '../utilities/init-firebase'
+
+export const Createquiz = ( currentQuizId, title, description) => {
+    return db().collection('Quizzes').doc(currentQuizId).set({
+        title,
+        description,
+    });
+};
